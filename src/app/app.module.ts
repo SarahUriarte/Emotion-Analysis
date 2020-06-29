a57ad4b8b7e512276d8651d97d5a76f8afe4f663
Ignacio Cruz Montero <ignacio1999montero@hotmail.com>
2020-06-29 13:43:33 -0600

    Example menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,12 @@ import { AppComponent } from './app.component';
 import { DataService } from './data.service';
 import {RouterModule} from '@angular/router';
 // tslint:disable-next-line:no-unused-expression
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EstudiantesComponent } from './estudiantes/estudiantes.component';
 import { ComputacionComponent } from './computacion/computacion.component';
@@ -21,7 +26,11 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    MatDialogModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
     RouterModule.forRoot(ROUTES),
     BrowserAnimationsModule,
 
