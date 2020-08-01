2b371c64e6d57bc6a22f190f4239ac2611ad09c8
Ignacio Cruz Montero <ignacio1999montero@hotmail.com>
2020-08-01 16:37:57 -0600

    Cambios de CSS

--- src/app/enviroment/environment.ts
+++ src/app/enviroment/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  API_ROOT: 'http://localhost:8080',
+  API_ROOT: 'http://34.72.57.203:8080',
   SUBSCRIPTION_KEY: 'b11663ca7b4640b68d85ccca56bb2252',
   FACE_URL: 'https://face-recognition-tec.cognitiveservices.azure.com/face/v1.0/detect?returnFaceAttributes=emotion'
 };
